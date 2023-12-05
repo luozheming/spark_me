db4cd76c727a87e489bb37ae8642d05a993f99b6
richardluo <lzm931105@163.com>
2023-12-05 20:36:15 +0800

    "add java connection"

--- src/java/com/clickhouse/StartClickHouse.java
+++ src/java/com/clickhouse/StartClickHouse.java
@@ -1,14 +1,19 @@
 package com.clickhouse;
 
-
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@MapperScan(basePackages = {"com.clickhouse.mapper"})
+@RestController
 public class StartClickHouse {
     public static void main(String[] args) {
-        SpringApplication.run(StartClickHouse.class);
+        SpringApplication.run(StartClickHouse.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 }
