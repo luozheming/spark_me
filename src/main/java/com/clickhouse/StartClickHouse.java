5c4f5ae65da53dd8462a671c59eb175b4a34d62a
richard <lzm931105@163.com>
2023-12-17 21:56:29 +0800

    "clickhouse config add"

--- src/main/java/com/clickhouse/StartClickHouse.java
+++ src/main/java/com/clickhouse/StartClickHouse.java
@@ -1,11 +1,9 @@
 package com.clickhouse;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-
 public class StartClickHouse {
     public static void main(String[] args) {
         SpringApplication.run(StartClickHouse.class, args);
